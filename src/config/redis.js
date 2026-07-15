fd758d61f3d710bb090830412622fd78a9b09200
ReviseGithub <sandeep.rajput122001@gmail.com>
2026-07-15 11:50:38 +0530

    fix redis community service

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,14 +1,21 @@
-import Redis from 'ioredis';
-import logger from '../utils/logger.js';
+import Redis from "ioredis";
+import logger from "../utils/logger.js";
 
 const redis = new Redis(process.env.REDIS_URL, {
-  maxRetriesPerRequest: 3,
-  retryStrategy: (times) => Math.min(times * 50, 2000),
-  lazyConnect: true,
+  maxRetriesPerRequest: 1,
+  enableReadyCheck: false,
+  connectTimeout: 5000,
+  commandTimeout: 3000,
+  retryStrategy(times) {
+    if (times > 3) return null;
+    return Math.min(times * 100, 3000);
+  },
 });
 
-redis.on('connect',      () => logger.info('Redis connected'));
-redis.on('error',        (err) => logger.error('Redis error:', err.message));
-redis.on('disconnected', () => logger.warn('Redis disconnected'));
+redis.on("connect", () => logger.info("Redis connected"));
+redis.on("ready",   () => logger.info("Redis ready"));
+redis.on("error",   (err) => {
+  logger.error("Redis error", { message: err.message });
+});
 
 export default redis;
