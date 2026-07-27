bb816b82fb450dd2221bc50956b9131570782771
ReviseGithub <sandeep.rajput122001@gmail.com>
2026-07-27 19:25:59 +0530

    fix socket issue

--- server.js
+++ server.js
@@ -4,16 +4,13 @@ import app from "./app.js";
 import connectDB from "./src/config/db.js";
 import initSocket from "./src/socket.js";
 import logger from "./src/utils/logger.js";
-
 const PORT = process.env.PORT || 3004;
-
 const start = async () => {
   try {
     await connectDB();
-
     const httpServer = http.createServer(app);
     const io = initSocket(httpServer);
-
+    app.set("io", io);  
     httpServer.listen(PORT, () => {
       logger.info(`━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`);
       logger.info(`  Community Service running on port ${PORT}`);
@@ -28,14 +25,11 @@ const start = async () => {
     process.exit(1);
   }
 };
-
 start();
-
 process.on("unhandledRejection", (err) => {
   logger.error("Unhandled Rejection:", err.message);
   process.exit(1);
 });
-
 process.on("uncaughtException", (err) => {
   logger.error("Uncaught Exception:", err.message);
   process.exit(1);
